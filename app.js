fd943b4153b0d833ff0f3a193a9807a8e15894da
Shelby Norris <shelbs9109@gmail.com>
2025-07-01 20:47:02 -0500

    added JS classes syntax to app.js file, edited headers in index.html file

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// *********CONSTRUCTOR FUNCTIONS**********
+
 /**
  * 
  * @param {string} name - common name of the snake 
@@ -39,3 +41,61 @@ ballPython.checkIfVenomous();
 console.log(gaboonViper);
 gaboonViper.checkIfVenomous();
 
+
+// **********OBJECT CLASSES - ES6 SYNTAX**********
+
+class NorthAmericanPredator{
+/**
+ * 
+ * @param {string} name - common name for animal
+ * @param {string} vertebrateClass - mammal, reptile, amphibian
+ * @param {boolean} hasFur - does it have fur
+ * @param {string} habitat - general habitat
+ * @param {number} age - made up individual animal's age for birthday purposes
+ */
+    constructor(name, vertebrateClass, hasFur, habitat, age) {
+        this.name = name;
+        this.vertebrateClass = vertebrateClass;
+        this.hasFur = hasFur;
+        this.habitat = habitat;
+        this.age = age;
+    }
+
+    greet() {
+       const greeting = (`Hello! I'm the ${this.name}!`)
+        return greeting;
+    }
+
+    haveBirthday() {
+        this.age += 1;
+        // console.log(`Happy birthday to the ${this.name}! They're ${this.age} years old! `)
+        return this.age
+    }
+}
+
+
+const greyWolf = new NorthAmericanPredator(
+    "Grey Wolf",
+    "mammal",
+    true,
+    "forests",
+    5
+);
+
+const americanAlligator = new NorthAmericanPredator(
+    "American Alligator",
+    "reptile",
+    false,
+    "wetlands",
+    15
+);
+
+console.table(greyWolf); // I tried this here because the devtools console was logging things in alphabetical order and not the order I have them coded **** I came back 10 minutes later and now it's being normal, but this looks nice so it's staying!
+console.log(americanAlligator);
+
+
+console.log(greyWolf.greet());
+console.log(`Happy Birthday to the ${greyWolf.name}! They're ${greyWolf.haveBirthday()} today!`);
+
+console.log(americanAlligator.greet());
+console.log(`Happy Birthday to the ${americanAlligator.name}! They're ${americanAlligator.haveBirthday()} today!`);
